6f54ae515e2550da1e94703f1ba5880a0ab6862b
mr-z <mo.erfan1379@gmail.com>
2020-11-23 12:54:23 +0330

    pasandaz

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <math.h>
 int main()
 {
 	int a;
@@ -18,7 +18,23 @@ int main()
 	scanf("%d", &hour);
 	s = (hour / (365 * 24));
 	rooz = (hour % (365 * 24) / 24);
-	printf("%d %d", s, rooz);
-
+	printf("%d %d\n", s, rooz);
+	//be ezae har  1nomre ke moadelesh kamtar az 20 bashe 100toman jarime mishe. hala edame barname mohasebe meqdar sal mored niaz bararye jobran zarare
+	int p=1000,j=0;
+	double k,t=1;
+	p=1000-((20-m)*100);
+	if(p<=0){
+		printf("badbakht:(");
+		j=1;
+	}
+	k=p*(pow(1.5,t));
+	while (k<=1000)
+	{
+		t=t+1;
+		k=p*(pow(1+0.5,t));
+		
+	}
+	if(j==0)
+		printf( "%lf sal bayad montazer bashe",t);
 	return (0);
 }
